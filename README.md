a0495fb2af13183460d3acdaaff26874698854fd
amaia <alexandre.maia@gmail.com>
2016-12-08 00:15:52 -0200

    woaaah

--- README.md
+++ README.md
@@ -6,4 +6,17 @@ This is my attempt at automating part of my workflow using AWS to help working o
 
 # Setup
 
-AWS credentials in ~/.boto
+- AWS credentials in ~/.boto
+- For batch jobs, Docker image in aamaia/kaggle-batch
+
+# TODO: batch workflow
+
+- create & configure s3 bucket if needed
+- launching spot instance
+- collect output
+
+
+# TODO: interactive workflow
+
+- prepare image with jupyter and letsencrypt
+
--- klaws/price.py
+++ klaws/price.py
@@ -5,15 +5,17 @@ import boto3
 import urllib.request
 import os
 import json
+from operator import itemgetter
 
 client = boto3.client('ec2')
 
-def history():
+def history(instace_types):
     data = client.describe_spot_price_history(
         DryRun=False,
-        StartTime=datetime(2016, 12, 1),
-        EndTime=datetime(2016, 12, 6),
-        #InstanceTypes=['t1.micro|t2.nano']
+        ProductDescriptions = ['Linux/UNIX', 'Linux/UNIX (Amazon VPC)'],
+        StartTime=datetime(2016, 12, 7, 20, 59),
+        EndTime=datetime(2016, 12, 7, 23, 59),
+        InstanceTypes=instace_types
     )
     return data
     
@@ -25,6 +27,7 @@ def instance_types():
         return json.load(f)
     
 def filter_instances(h):
+    result = {}
     for k in h:
         itype = k['instance_type']
         #iname = k['pretty_name']
@@ -35,15 +38,28 @@ def filter_instances(h):
         else:
             istorage_size = istorage['size']
         imem = k['memory']
+        icpu = [k['ECU'], k['vCPU']]
         #max on-demand price across all AZs
         prices = [values.get('linux').get('ondemand') for zone, values in ipricing.items()]
         if len(prices) > 0:
             iprice_max = max(prices)
             iprice_min = min(prices)   
-            print(','.join([itype, str(iprice_min), str(iprice_max), str(imem), str(istorage_size)]))
+            result[itype] = {'price': [iprice_min, iprice_max], 'mem': imem, 'storage': istorage_size, 'cpu': icpu}            
+    return result
         
-if __name__ == '__main__':
-    types = instance_types()
-    filter_instances(types)
-    #x = history()
+if __name__ == '__main__':    
+    itypes = filter_instances(instance_types())
+    ordered = [[k, v['price'][0], v['mem'], v['cpu']] for k, v in itypes.items() if v['mem'] >= 8 and v['mem'] <= 32]
+    for k in sorted(ordered, key=lambda x: float(x[1])):
+        print(k)
+        
+    
+    selected_types = ['m4.large'] #['t2.large', 'm4.large']
+    x = history(selected_types)
+    for item in x['SpotPriceHistory']:
+        az, itype, desc, price, ts = item['AvailabilityZone'], item['InstanceType'], item['ProductDescription'], item['SpotPrice'], item['Timestamp']
+        if itype == 'm4.large':
+            print(ts, az, price)
+
+    
     
